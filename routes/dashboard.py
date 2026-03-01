323c2635c301b5be1024acf6e93b3819be00aaac
viktoriyabaran <viktoriabaran83@gmail.com>
2026-03-01 22:53:03 +0200

    Make the FaaS endpoint a configurable variable

--- routes/dashboard.py
+++ routes/dashboard.py
@@ -1,7 +1,8 @@
+import os
 from flask import Blueprint, render_template
 
 dashboard_bp = Blueprint('dashboard', __name__)
 
 @dashboard_bp.route('/dashboard')
 def dashboard():
-    return render_template('dashboard.html')
+    return render_template('dashboard.html', api_url=os.environ.get('ANALYTICS_API_URL', ''))
