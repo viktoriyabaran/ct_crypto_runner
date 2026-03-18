3db9c6a65ec5fa46f71efd7820a3e544b9f55283
viktoriyabaran <viktoriabaran83@gmail.com>
2026-03-18 18:14:57 +0200

    Wrong dependency lol

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import threading
 
 from flask import Flask
-from prometheus_flask_instrumentator import Instrumentator
+from prometheus_flask_exporter import PrometheusMetrics
 
 from config import PORT
 from routes import dashboard_bp, health_bp, price_bp
@@ -12,7 +12,7 @@ app.register_blueprint(health_bp)
 app.register_blueprint(price_bp)
 app.register_blueprint(dashboard_bp)
 
-Instrumentator().instrument(app).expose(app)
+metrics = PrometheusMetrics(app)
 
 if __name__ == '__main__':
     thread = threading.Thread(target=run_heartbeat, daemon=True)
